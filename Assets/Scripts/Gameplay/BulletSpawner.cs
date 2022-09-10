53867f7f69a87bd2f04288170bc156d0012681ad
mmaimer33 <rchousalkar@gmail.com>
2022-09-10 17:41:56 +0800

    Created bullet spawner script.

--- Assets/Scripts/Gameplay/BulletSpawner.cs
+++ Assets/Scripts/Gameplay/BulletSpawner.cs
@@ -6,23 +6,26 @@ public class BulletSpawner : MonoBehaviour
 {
     #region Fields
 
-    public float frequency; // How often a bullet spawns
+    public float frequency = 2f; // How often a bullet spawns
     public Direction direction; // Which direction the bullet goes in
 
     [SerializeField]
     private GameObject bulletPrefab;
+    private Vector3 bulletPosition;
+    private GameObject bullet;
 
     #endregion
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        bulletPosition = transform.position;
+        InvokeRepeating("SpawnBullet", 0, frequency);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void SpawnBullet()
     {
-        
+        bullet = Instantiate<GameObject>(bulletPrefab, bulletPosition, transform.rotation);
+        bullet.GetComponent<BulletController>().direction = direction;
     }
 }
